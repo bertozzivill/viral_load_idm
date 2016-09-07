015350c5e439d2e62a92960e36ec3461d171be86
Amelia Bertozzi-Villa <abertozzivilla@intven.com>
2016-09-07 11:30:34 -0700

    Fix bug that was calculating age quintiles differently in test and train datasets

--- calculate_rmse.r
+++ calculate_rmse.r
@@ -30,7 +30,8 @@ library(reshape2)
   #generate uniquely-names columns for each type of age used (continuous, binned, etc)
   setnames(test_data, "agesero", "agesero_cont")
   test_data[, agesero_bin_10:= cut(agesero_cont, breaks=c(15, 25, 35, 45, Inf), labels=c("15-25", "25-35", "35-45", "45+"))]
-  test_data[, agesero_quint:= cut(agesero_cont, breaks=5, labels=c("quint_1", "quint_2", "quint_3", "quint_4", "quint_5"))]
+  # the values for the breaks here correspond to quintiles of the original dataset
+  test_data[, agesero:= cut(agesero_cont, breaks=c(15.4, 28.2, 41, 53.8, 66.6, Inf), labels=c("quint_1", "quint_2", "quint_3", "quint_4", "quint_5"))]
   test_data[, agesero_none:= agesero_cont]
   
   ##build rmse framework
